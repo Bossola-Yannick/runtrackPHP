5eb392be9f8c01d1044ef5e097d04ea7e69f6a74
Bossola-Yannick <bossolay@hotmail.fr>
2025-01-14 11:49:11 +0100

    fin jour02/job05

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,6 +1,6 @@
 <?php
 for ($i = 0; $i <= 1337; $i++) {
     if (($i == 26) or ($i == 37) or ($i == 88) or ($i == 111)) {
-        echo "<p>**</p>";
+        echo "<p></p>";
     } else echo "<p>$i</p>";
 }
---
+++ jour02/job05/index.php
@@ -0,0 +1,16 @@
+<?php
+function isPrime($number)
+{
+    for ($i = 2; $i < $number; $i++) {
+        if ($number % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+for ($i = 3; $i < 100; $i++) {
+    if (isPrime($i)) {
+        echo $i . " est un nombre primaire<br>";
+    }
+}
